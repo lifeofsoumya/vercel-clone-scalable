3b34dc7b65fe1dc739120e7597072a63ed4bb36d
soumya <mondalsoumya2001@gmail.com>
2024-02-12 23:46:57 +0530

    s3 uploading for all the file iteratively

--- build_server/main.sh
+++ build_server/main.sh
@@ -2,7 +2,7 @@
 
 export GIT_REPOSITORY_URL="$GIT_REPOSITORY_URL"
 
-git clone "$GIT_REPOSITORY_URL" ./home/app/output
+git clone --depth 1 "$GIT_REPOSITORY_URL" ./home/app/output
 
 cd ./home/app/output
 
@@ -14,8 +14,11 @@ npm run build
 echo "Build Process completed <0_0>"
 
 # cd dist
-# echo "Listing down the dist directory"
+# echo "Listing down the dist directory >>"
 # ls -a
+
+# echo "Compressing dist folder..."
+# tar -czf dist.tar.gz ./dist #skipped zip and upload as it would need a lambda to unzip then again put to s3
 
 ## going back from dist > output > build_server
 pwd
--- build_server/script.js
+++ build_server/script.js
@@ -1,6 +1,17 @@
 const { exec } = require("child_process");
 const path = require("path");
 const fs = require("fs");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const mime = require("mime-types");
+require("dotenv").config();
+
+const s3Client = new S3Client({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
 
 async function init() {
   const distDirComponents = fs.readdirSync(
@@ -9,5 +20,22 @@ async function init() {
   );
 
   console.log("distDirComponents", distDirComponents);
+
+  for (const filePath of distDirComponents) {
+    if (fs.lstatSync(filePath).isDirectory()) continue;
+
+    console.log('Starting the uploading process for ', filePath)
+
+    const command = new PutObjectCommand({
+      Bucket: "ocode",
+      Key: `__outputs/${project_id}/${filePath}`,
+      Body: fs.createReadStream(filePath),
+      ContentType: mime.lookup(filePath),
+    });
+
+    await s3Client.send(command);
+  }
+
+  console.log("Done uploading to S3");
 }
-init()
+init();
